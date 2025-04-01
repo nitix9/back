8c800cfc7bc6505c543f18d953f52709ff1924d7
Nikita <romashko.04@mail.ru>
2025-04-01 09:39:51 +0500

    исправлено под задание

--- main.py
+++ main.py
@@ -10,21 +10,23 @@ def read_root():
 @app.get("/about_me")
 def show_about_me(c:int,a:int = None,b:int=6):
     return{
-        'name':'Nikitos Bandito',
-        'years':'49',
-        'work':'Banditos',
-        'hobby':'No hobby',
+        'name':'Nikita',
+        'lastname':'Romashko',
+        'patronymic':'Dmitrievich',
+        'years':'21',
+        'numgroup':'T-323901-NT',
         's':a+b,
     }
 
 @app.get('/random_num')
 def show_random_num():
-    return{'rand_num':random.randint(0,100)}
+    return{'rand_num':random.randint(1,10)}
 
-@app.get('/square_tr')
+@app.post('/t_square')
 def show_square_tr(a:int=Query(gt=0),b:int=Query(gt=0),c:int=Query(gt=0)):
     p=(a+b+c)/2
     if (a>b+c or b>a+c or c>b+a):
         return {'Ошибка':'Сторона не может быть больше суммы двух сторон'}
     else:
-        return{'square':math.sqrt(p*(p-a)*(p-b)*(p-c))}
+        return{'square':math.sqrt(p*(p-a)*(p-b)*(p-c)),
+                'perimeter':p}
